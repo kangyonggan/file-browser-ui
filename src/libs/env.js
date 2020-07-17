5c68ef9f15f8728360323f9ba18a8e4fa8fe4b23
kangyonggan <java@kangyonggan.com>
2020-07-17 17:49:18 +0800

    hd deploy

--- src/libs/env.js
+++ src/libs/env.js
@@ -8,6 +8,8 @@ axios.defaults.baseURL = '/file-browser/';
 
 if (process.env.NODE_ENV === 'development') {
     axios.defaults.baseURL = 'http://localhost:8805/';
+} else if (process.env.NODE_ENV === 'hd') {
+    axios.defaults.baseURL = 'http://172.190.105.90/file-browser/';
 }
 
 export default env;
